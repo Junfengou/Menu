807f484fdfdf8e981237d7d459fa4d6ab7e12dea
Jun Ou <jun.ou@quantumworkplace.com>
2023-05-03 17:48:00 -0500

    first commit

---
+++ Menu/Controllers/APIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Menu.Controllers
+{
+    public class APIController : Controller
+    {
+		// GET: API -> http://localhost:44377/API/GetData
+		public ActionResult GetData()
+		{
+			List<Person> Persons = new List<Person>();
+			Persons.Add(new Person { Name = "Paddy", FavouriteColour = "Blue" });
+			Persons.Add(new Person { Name = "Frank", FavouriteColour = "Brown" });
+
+			return Json(Persons, JsonRequestBehavior.AllowGet);
+		}
+
+		public class Person
+		{
+			public string Name { get; set; }
+			public string FavouriteColour { get; set; }
+		}
+	}
+}
---
+++ Menu/React/src/App.js
@@ -0,0 +1,55 @@
+﻿import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+/*
+    For whatever reason. When running in IIS express, the pop up browser will not hot reload. 
+    Hot reload only work if you're looking at the "Real Browser"
+
+    Just manually type in the URL: http://localhost:44377/
+
+    [webpack --mode development --watch] still work for hot reload 😄
+*/
+
+function App() {
+    const [persons, setPersons] = useState([]);
+    const APIUrl = '/API/GetData';
+
+    const getData = async () => {
+        const res = await axios(
+            APIUrl
+        );
+        setPersons(res.data);
+    }
+
+    console.log("yo")
+    
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <>
+            <h1>People that we know</h1>
+            {persons && persons.length > 0 &&
+                <table style={{ borderCollapse: "collapse" }}>
+                    <thead>
+                        <tr>
+                            <th style={{ border: "solid 1px black" }}>Name</th>
+                            <th style={{ border: "solid 1px black" }}>Favourite Colour</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {persons.map(p => {
+                            return <tr key={p.Name}>
+                                <td style={{ border: "solid 1px black" }}>{p.Name}</td>
+                                <td style={{ border: "solid 1px black" }}>{p.FavouriteColour}</td>
+                            </tr>
+                        })
+                        }
+                    </tbody>
+                </table>
+            }
+        </>
+    );
+}
+export default App;
